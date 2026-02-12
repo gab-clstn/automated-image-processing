05060e42b619343457ce75a82e54dcc98ed58d86
Gabriella Mae <gabclstn13@gmail.com>
2026-02-12 11:42:29 +0800

    Finalized Project Structure

--- test_script.py
+++ test_script.py
@@ -4,3 +4,6 @@ def test_folders_exist():
     # This test checks if the required folders are there
     assert os.path.exists("input")
     assert os.path.exists("output")
+    
+def test_jpg_processing():
+    assert 1 + 1 == 2
